3c555b7a842d1088a0717fb9b932f1d949881f1e
Rodion <rodionserez@gmail.com>
2023-08-11 13:01:55 +0300

    feat: add router file

---
+++ src/router/index.tsx
@@ -0,0 +1,28 @@
+// import Main from 'pages/main/Main';
+// import Registration from 'pages/registration/Registration';
+// import Login from 'pages/login/Login';
+// import Error from 'pages/error/Error';
+
+// interface route {
+//   path: string;
+//   component: React.ElementType;
+// }
+
+export enum pagePathnames {
+  MAIN = '/main',
+  REGISTRATION = '/registration',
+  LOGIN = '/login',
+  ERROR = '/error',
+}
+
+// export const publicRoutes: route[] = [
+//   { path: pagePathnames.MAIN, component: Main },
+//   { path: pagePathnames.ERROR, component: Error },
+// ];
+
+// export const authRoutes: route[] = [];
+
+// export const unauthRoutes: route[] = [
+//   { path: pagePathnames.REGISTRATION, component: Registration },
+//   { path: pagePathnames.LOGIN, component: Login },
+// ];
