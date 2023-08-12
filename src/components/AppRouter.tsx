80d6e652834268403a928a8a7381731bacb7eda4
Rodion <rodionserez@gmail.com>
2023-08-12 10:01:34 +0300

    feat: add new routes

--- src/components/AppRouter.tsx
+++ src/components/AppRouter.tsx
@@ -1,12 +1,16 @@
+import React from 'react';
 import { AuthProvider } from 'hocs/AuthProvider';
 import Main from 'pages/main/Main';
 import Login from 'pages/login/Login';
 import Registration from 'pages/registration/Registration';
 import Error from 'pages/error/Error';
-import React from 'react';
 import { Route, Routes, Navigate } from 'react-router-dom';
 import { pagePathnames } from 'router';
 import { CheckAuth } from 'hocs/CheckAuth';
+import About from 'pages/about/About';
+import Catalog from 'pages/catalog/Catalog';
+import UserProfile from 'pages/user-profile/UserProfile';
+import Basket from 'pages/basket/Basket';
 
 const AppRouter = () => {
   return (
@@ -14,6 +18,8 @@ const AppRouter = () => {
       <Routes>
         <Route path={pagePathnames.MAIN} element={<Main />} />
         <Route path="/" element={<Navigate to={pagePathnames.MAIN} />} />
+        <Route path={pagePathnames.ABOUT} element={<About />} />
+        <Route path={pagePathnames.CATALOG} element={<Catalog />} />
         <Route
           path={pagePathnames.LOGIN}
           element={
@@ -30,6 +36,15 @@ const AppRouter = () => {
             </CheckAuth>
           }
         />
+        <Route
+          path={pagePathnames.USER_PROFILE}
+          element={
+            <CheckAuth>
+              <UserProfile />
+            </CheckAuth>
+          }
+        />
+        <Route path={pagePathnames.BASKET} element={<Basket />} />
         <Route path={pagePathnames.ERROR} element={<Error />} />
         <Route path="/*" element={<Navigate to={pagePathnames.ERROR} />} />
       </Routes>
