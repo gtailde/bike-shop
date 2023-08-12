701c3c593d17c3ea2b163fbe0053f6bef0b0468d
Rodion <rodionserez@gmail.com>
2023-08-12 10:02:42 +0300

    feat: mount Header to the page

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import './styles/app.scss';
 import AppRouter from 'components/AppRouter';
+import Header from 'components/header/Header';
 
 function App() {
   return (
     <React.StrictMode>
       <BrowserRouter>
+        <Header></Header>
         <AppRouter></AppRouter>
       </BrowserRouter>
     </React.StrictMode>
