06726d875e1d80cdf368420ea240dafa5bc99643
Rodion <rodionserez@gmail.com>
2023-08-12 21:27:49 +0300

    feat: delete unnecessary code

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,15 +1,5 @@
-// import Main from 'pages/main/Main';
-// import Registration from 'pages/registration/Registration';
-// import Login from 'pages/login/Login';
-// import Error from 'pages/error/Error';
-
-// interface route {
-//   path: string;
-//   component: React.ElementType;
-// }
-
 export enum pagePathnames {
-  MAIN = '/main',
+  MAIN = '/',
   ABOUT = '/about',
   CATALOG = '/catalog',
   REGISTRATION = '/registration',
@@ -18,15 +8,3 @@ export enum pagePathnames {
   BASKET = '/basket',
   ERROR = '/error',
 }
-
-// export const publicRoutes: route[] = [
-//   { path: pagePathnames.MAIN, component: Main },
-//   { path: pagePathnames.ERROR, component: Error },
-// ];
-
-// export const authRoutes: route[] = [];
-
-// export const unauthRoutes: route[] = [
-//   { path: pagePathnames.REGISTRATION, component: Registration },
-//   { path: pagePathnames.LOGIN, component: Login },
-// ];
