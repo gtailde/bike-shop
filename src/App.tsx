9c24ab8f34f47e4a75c06775dbabcb3b2e6bad15
Rodion <rodionserez@gmail.com>
2023-08-12 22:11:58 +0300

    refactor: delete extra end tags, change paths

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import './styles/app.scss';
-import AppRouter from 'components/AppRouter';
+import './styles/App.scss';
+import AppRouter from 'router/AppRouter';
 import Header from 'components/header/Header';
 
 function App() {
   return (
     <React.StrictMode>
       <BrowserRouter>
-        <Header></Header>
-        <AppRouter></AppRouter>
+        <Header />
+        <AppRouter />
       </BrowserRouter>
     </React.StrictMode>
   );
