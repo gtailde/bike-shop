3779b0457f391622a99ce876430dcf38ac0be3df
Rodion <rodionserez@gmail.com>
2023-08-13 22:45:22 +0300

    feat: add 404 error page

--- src/pages/error/Error.tsx
+++ src/pages/error/Error.tsx
@@ -1,5 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './style.scss';
+import { CustomButton } from 'components/UI/custom-button/CustomButton';
+import { pagePathnames } from 'router/pagePathnames';
 
 export const Error = () => {
-  return <div> Error </div>;
+  return (
+    <section className="error-section">
+      <h1 className="error-header"> 404 Error </h1>
+      <h3 className="error-text">The page you were looking for counldn&apos;t be found.</h3>
+      <Link to={pagePathnames.main}>
+        <CustomButton className=""> Back to main page </CustomButton>
+      </Link>
+    </section>
+  );
 };
