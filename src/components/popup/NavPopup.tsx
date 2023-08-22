7de6b2c812295770a03d5c01e9f5be107d84a694
Dima Grigorev <firebrand-rsschool@rambler.ru>
2023-08-22 23:29:37 +0300

    fix: popup position

--- src/components/popup/NavPopup.tsx
+++ src/components/popup/NavPopup.tsx
@@ -1,4 +1,4 @@
-import React, { type ReactNode, type FC } from 'react';
+import React, { type ReactNode, type FC, useEffect, useRef } from 'react';
 import './style.scss';
 
 export interface Props extends Omit<Partial<HTMLDivElement>, 'children'> {
@@ -8,11 +8,34 @@ export interface Props extends Omit<Partial<HTMLDivElement>, 'children'> {
 }
 
 export const NavPopup: FC<Props> = ({ isOpened, onClose, children }) => {
+  const popupRef = useRef<HTMLElement>(null);
+  useEffect(() => {
+    const userNavigation = document.querySelector('.page-header__user-navigation') as HTMLElement;
+    const setPosition = () => {
+      const positionTop = userNavigation.offsetTop + userNavigation.offsetHeight;
+      const positionLeft = userNavigation.offsetLeft;
+      if (popupRef.current) {
+        popupRef.current.style.top = `${positionTop}px`;
+        popupRef.current.style.left = `${positionLeft}px`;
+      }
+    };
+
+    setPosition();
+
+    window.addEventListener('resize', setPosition);
+
+    return () => {
+      window.removeEventListener('resize', setPosition);
+    };
+  }, [isOpened]);
+
   return (
     isOpened && (
       <div className="popup">
-        <div className="nav-popup__overlay" onClick={onClose}></div>
-        <section className="nav-popup">{children}</section>
+        <div className="popup__overlay" onClick={onClose}></div>
+        <section ref={popupRef} className="nav-popup">
+          {children}
+        </section>
       </div>
     )
   );
