eb2b81cacac8628f945c232d181ee228dfe97c78
Dima Grigorev <firebrand-rsschool@rambler.ru>
2023-08-25 18:31:11 +0300

    feat: add select component

---
+++ src/components/UI/Select/Select.tsx
@@ -0,0 +1,26 @@
+import './style.scss';
+import React, { type ComponentProps } from 'react';
+import { ReactComponent as ArrowDownIcon } from './assets/arrow-down-icon.svg';
+import { ReactComponent as SortIcon } from './assets/sort-icon.svg';
+
+export const Select = ({ className, ...props }: ComponentProps<'select'>) => {
+  return (
+    <div className={`${className ?? ''} select`}>
+      <select {...props}>
+        <option value="none" disabled>
+          Sort by:
+        </option>
+        <option value="price-asc">Price ↑</option>
+        <option value="price-desc">Price ↓</option>
+        <option value="name-asc">Name ↑</option>
+        <option value="name-desc">Name ↓</option>
+      </select>
+      <div className="select__icon-container select__icon-container--left">
+        <SortIcon className="icon" />
+      </div>
+      <div className="select__icon-container">
+        <ArrowDownIcon className="icon" />
+      </div>
+    </div>
+  );
+};
