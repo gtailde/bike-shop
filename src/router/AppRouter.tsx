a9355d11b96acca7b4cce7f741f57fb0eb011912
Dima Grigorev <firebrand-rsschool@rambler.ru>
2023-08-25 18:44:09 +0300

    feat: add product details page template

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -10,6 +10,7 @@ import { About } from 'pages/about/About';
 import { CatalogPage } from 'pages/catalog/Catalog';
 import { UserProfile } from 'pages/user-profile/UserProfile';
 import { Basket } from 'pages/basket/Basket';
+import { ProductDetailsPage } from 'pages/ProductDetails/ProductDetailsPage';
 import { RequireAuth } from 'hocs/RequireAuth';
 
 const AppRouter = () => {
@@ -18,6 +19,7 @@ const AppRouter = () => {
       <Route path={pagePathnames.main} element={<Main />} />
       <Route path={pagePathnames.about} element={<About />} />
       <Route path={pagePathnames.catalog} element={<CatalogPage />} />
+      <Route path={pagePathnames.product} element={<ProductDetailsPage />} />
       <Route
         path={pagePathnames.login}
         element={
