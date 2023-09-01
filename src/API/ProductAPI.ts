4f0418d23adab504684b70c4d53cdc488fdcd8de
Oleg Nepiuk <gtailde@gmail.com>
2023-09-01 14:57:20 +0200

    feat: add product search method

--- src/API/ProductAPI.ts
+++ src/API/ProductAPI.ts
@@ -22,6 +22,26 @@ class ProductAPI extends CommercetoolsAPI {
   public async getProducts() {
     return await this.performGetRequest('products');
   }
+
+  public async searchProduct(searchText: string, limit = 10, offset = 0): Promise<string> {
+    try {
+      const token = this.getToken();
+      const url = `${this.apiUrl}/${this.projectKey}/product-projections/search`;
+
+      const queryParams = new URLSearchParams({
+        [`text.en-US`]: searchText,
+        limit: limit.toString(),
+        offset: offset.toString(),
+      }).toString();
+
+      const headers = this.getTokenHeaders(token.access_token);
+      const response = await axios.get(`${url}?${queryParams}`, { headers });
+      return response.data;
+    } catch (error) {
+      console.error('An unexpected error occurred:', error);
+      throw new Error('Error searching product projections by text');
+    }
+  }
 }
 
 const productAPI = new ProductAPI();
