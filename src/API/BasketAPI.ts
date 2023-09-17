81620b70de75dba3afb00f5f3bbb9ccdc738ee27
Oleg Nepiuk <gtailde@gmail.com>
2023-09-17 15:46:16 +0200

    fix: issue in the deleteCart method

--- src/API/BasketAPI.ts
+++ src/API/BasketAPI.ts
@@ -10,19 +10,20 @@ class BasketAPI extends CommercetoolsAPI {
   ): Promise<ICart | ICartList> {
     try {
       const token = this.getToken();
-      const url = `${this.apiUrl}/${this.projectKey}/me/${endpoint}`;
+      let url = `${this.apiUrl}/${this.projectKey}/me/${endpoint}`;
       const headers = this.getTokenHeaders(token.access_token);
 
       let response;
+
+      if (requestData?.queryParams) url += `?${requestData.queryParams}`;
 
       if (request === 'get') {
         response = await axios.get(url, { headers });
       } else if (request === 'post' && requestData?.body) {
         response = await axios.post(url, requestData.body, { headers });
-      } else if (request === 'delete' && requestData?.queryParams) {
-        response = await axios.delete(`${url}?${String(requestData.queryParams)}`);
+      } else if (request === 'delete') {
+        response = await axios.delete(url, { headers });
       }
-
       return response?.data;
     } catch (error) {
       console.error('An unexpected error occurred:', error);
@@ -38,7 +39,7 @@ class BasketAPI extends CommercetoolsAPI {
     return (await this.performRequest('carts', 'post', { body: { currency: 'USD' } })) as ICart;
   }
 
-  public async deleteCart(cartIdent?: string, type?: 'id' | 'key'): Promise<void> {
+  public async deleteCart(cartIdent?: string, type?: 'id' | 'key'): Promise<ICart> {
     let cartI: string, cartV: number, typeI: string;
     if (cartIdent && type) {
       const getCart = await this.getCart(cartIdent, type);
@@ -48,17 +49,20 @@ class BasketAPI extends CommercetoolsAPI {
       [cartI, cartV, typeI] = [getMyCart.id, getMyCart.version, 'id'];
     }
 
-    // Включаем cart-id и cart-version в URL для DELETE запроса
-    await this.performRequest(`carts/${cartI}`, 'delete', {
-      queryParams: `version=${cartV}`,
-    });
+    return (await this.performRequest(
+      `carts/${typeI === 'id' ? cartI : `key=${cartI}`}`,
+      'delete',
+      {
+        queryParams: `version=${cartV}`,
+      },
+    )) as ICart;
   }
 
   // public async updateCart(): Promise<void> {}
 
   public async getCarts(limit = 20, offset = 0): Promise<ICartList> {
-    // add
-    return (await this.performRequest(`carts`, 'get')) as ICartList;
+    const queryParams = `limit=${limit}&offset=${offset}`;
+    return (await this.performRequest(`carts`, 'get', { queryParams })) as ICartList;
   }
 
   public async getCart(cartIdent: string, type: 'id' | 'key'): Promise<ICart> {
