85a67f271e126bc19a6f4f000bf636eff64793d8
Rodion <rodionserez@gmail.com>
2023-09-21 22:44:30 +0300

    feat: implement tracking user login status

--- src/App.tsx
+++ src/App.tsx
@@ -11,23 +11,31 @@ import basketAPI from 'API/BasketAPI';
 
 export const UserContext = createContext<{
   profileInfo?: ICustomer;
+  isUserLoggedIn?: boolean;
+  setIsUserLoggedIn?: React.Dispatch<React.SetStateAction<boolean>>;
   cart?: ICart;
   setCart?: React.Dispatch<React.SetStateAction<ICart | undefined>>;
 }>({});
 
 function App() {
   const [profileInfo, setProfileInfo] = useState<ICustomer>();
+  const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
   const [cart, setCart] = useState<ICart>();
 
   useEffect(() => {
     void getCustomerData();
-  }, []);
+    console.log('App effect');
+  }, [isUserLoggedIn]);
 
   const getCustomerData = async () => {
     try {
+      setProfileInfo(undefined);
+      setCart(undefined);
       const customer = await customersApi.getCustomer();
       if ('id' in customer) {
         setProfileInfo(customer);
+        setIsUserLoggedIn(true);
+        console.log('there is user');
         setCart(await basketAPI.getActiveCart());
       }
     } catch (error) {
@@ -36,7 +44,7 @@ function App() {
   };
 
   return (
-    <UserContext.Provider value={{ profileInfo, cart, setCart }}>
+    <UserContext.Provider value={{ profileInfo, isUserLoggedIn, setIsUserLoggedIn, cart, setCart }}>
       <BrowserRouter>
         <Header />
         <AppRouter />
